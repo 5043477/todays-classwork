9846fac5c18629c4b5f96e966e9f0a91f2c2940e
bwhitmore7 <bryson.whitmore@patelhs.org>
2020-12-27 20:59:59 +0000

    e

--- Main.java
+++ Main.java
@@ -1,5 +1,5 @@
 class Main {
   public static void main(String[] args) {
-    System.out.println("Hello, my name is" + name + " and i go to school at " + school + "and would you like my phone number, it is " + phoneNumber + "my email is" + email);
+    System.out.println("Hello, my name is" + Name + " and i go to school at " + School + "and would you like my phone number, it is " + PhoneNumber + "my email is" + Email);
   }
 }
--- Student.java
+++ Student.java
@@ -7,67 +7,66 @@
  {
      // Write 4 instance variables
   
-    private String name;
-    private String email;
-    private String phoneNumber;
-    private String school;
+    private String Name;
+    private String Email;
+    private String PhoneNumber;
+    private String School;
   
 
      // Write 3 constructors to initialize the instance variables
 public Student(String theFirst, String theLast) {
-       name = "James";
-       email = "james@patelhs.org";
-       phoneNumber = "813-256-3317";
-       school = "Dr. Kiran C Patel";
+       Name = "James";
+       Email = "james@patelhs.org";
+       PhoneNumber = "813-256-3317";
+       School = "Dr. Kiran C Patel";
      }
      //  1. no parameters using default values
 public Student()
 {
-   name = "James";
-   email = "james@patelhs.org";
-   phoneNumber = "813-256-3317";
-   school = "Dr. Kiran C Patel";
+   Name = "James";
+   Email = "james@patelhs.org";
+   PhoneNumber = "813-256-3317";
+   School = "Dr. Kiran C Patel";
 }
      //  2. 1 parameter and the rest default values
-public Student(String name, String email, String phoneNumber, String initschool)
+public Student(String Name, String Email, String PhoneNumber, String initschool)
 {
-   name = "James";
-   email = "james@patelhs.org";
-   phoneNumber = "813-256-3317";
-   school = initschool;
+   Name = "James";
+   Email = "james@patelhs.org";
+   PhoneNumber = "813-256-3317";
+   School = initSchool;
 }
      //  3. 4 parameters
-     public Student(String initname, String initemail, String initphoneNumber, initString initschool)
+     public Student(String initName, String initEmail, String initPhoneNumber,initString initSchool)
      {
-       name = initname;
-       email = initemail;
-       phoneNumber = initphoneNumber;
-       school = initschool;
+       Name = initName;
+       Email = initEmail;
+       PhoneNumber = initPhoneNumber;
+       School = initSchool;
      }
 
      // Write a print method that prints all the instance variables
      public void print()
      {
-      System.out.println("my name is " + name);
-      System.out.println("my email is " + email);
-      System.out.println("My phone number is" + phoneNumber);
-      System.out.println("i go to school at " + school);
+      System.out.println("my name is " + Name);
+      System.out.println("my email is " + Email);
+      System.out.println("My phone number is" + PhoneNumber);
+      System.out.println("i go to school at " + School);
      }
 
     // main method
     public static void main(String[] args)
     {
        // Construct 3 Student objects using the 3 different constructors
- name n1 = new name();
- name n2 = new name("James");
- email e1 = new email();
- email e2 = new email("james@patelhs.org");
- phoneNumber pn1 = new phoneNumber();
- phoneNumber pn2 = new phoneNumber("813-256-3317");
-school s1 = new school();
-school s2 = new school("Dr. Kiran C Patel");
+ Name n1 = new Name();
+ Name n2 = new Name("James");
+ Email e1 = new Email();
+ Email e2 = new Email("james@patelhs.org");
+ PhoneNumber pn1 = new PhoneNumber();
+ PhoneNumber pn2 = new PhoneNumber("813-256-3317");
+School s1 = new School();
+School s2 = new School("Dr. Kiran C Patel");
        // call their print() methods
 
     }
  }
- 
